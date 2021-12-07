7d21c6aeed26563f1caf0478fef3612e3f172f6c
MathieuLaurent <mathieu.laurent1995@gmail.com>
2021-12-07 19:00:44 +0100

    Maj base.html.twig

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -15,4 +15,20 @@ class BlogController extends AbstractController
     public function home(){
         return $this->render('site/home.html.twig');
     }
+
+    /**
+     * @route("/streamerview", name="streamerview")
+     */
+
+    public function streamerview(){
+        return $this->render('site/streamerview.html.twig');
+    }
+
+     /**
+      * @route("/profil", name="profil")
+      */
+
+    public function profil(){
+        return $this->render('site/profil.html.twig');
+    }
 }
